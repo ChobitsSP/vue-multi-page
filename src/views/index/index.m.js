dece9fa465055655d64cbdff666dd996e15e661b
Chobits <chobits@live.cn>
2018-07-13 18:14:27 +0800

    1

--- src/views/index/index.m.js
+++ src/views/index/index.m.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './Index.vue'
+import store from '@/store/Index.ts'
 
 new Vue({
   el: '#app',
+  store,
   template: '<App/>',
   components: {
     App
--- src/views/login/index.m.js
+++ src/views/login/index.m.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './Index.vue'
+import store from '@/store/Index.ts'
+
+import './assets/app.css'
 
 new Vue({
   el: '#app',
+  store,
   template: '<App/>',
   components: {
     App
